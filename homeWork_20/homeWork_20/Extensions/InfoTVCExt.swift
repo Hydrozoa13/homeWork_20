cdba75f35493f2768036a7bc353611c0327d6228
Zhenya Loyko <e.lojko@inbox.ru>
2023-09-14 02:06:47 +0300

    Some logic

---
+++ homeWork_20/homeWork_20/Extensions/InfoTVCExt.swift
@@ -0,0 +1,15 @@
+//
+//  InfoTVCExt.swift
+//  homeWork_20
+//
+//  Created by Евгений Лойко on 14.09.23.
+//
+
+import Foundation
+
+extension InfoTVC {
+    
+    func saveInfo() {
+        
+    }
+}
---
+++ homeWork_20/homeWork_20/Models/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  homeWork_20
+//
+//  Created by Евгений Лойко on 13.09.23.
+//
+
+import Foundation
+
+struct Person {
+    
+    let name: String
+    let surname: String
+    let email: String
+    let number: String
+}
